7d4cb788979debb4541ca60e845a007b6f979181
fearyfiremast <fearyfiremast@gmail.com>
2026-02-21 20:21:44 -0800

    Fix: updated component to position itself in a fixed manner and touched up breakpoints

--- src/components/shared/StickyCTA.tsx
+++ src/components/shared/StickyCTA.tsx
@@ -9,19 +9,19 @@ const StickyCTA = () => {
         <>
         {/* Outer Container 
             - Internal tailwindcss classnames organized from top to bottom: shared rules, larger breakpoints -> smaller*/}
-        <div className="h-[84px] z-20 bg-blueprint-white py-[12px] pl-[36px] pr-[12px] rounded-[8px] shadow-[2px_4px_10px_0_rgba(0_0_0_0.07)]
-                        border-[1px] border-blueprint-black border-
-                        min-[461px]:w-[460px]
-                        max-[460px]:pl-[24px] max-[460px]:rounded-[5px]">
+        <div className="h-[84px] z-20 bg-blueprint-white py-[12px] pl-[36px] pr-[12px] rounded-[8px] shadow-[2px_4px_10px_0_rgba(0_0_0_0.07)] fixed bottom-0 right-0 w-[100%]
+                        min-[460.02px]:w-[460px]
+                        max-[460.01px]:pl-[24px] max-[460.01px]:rounded-[5px]">
             {/* Content Flex Row Container */}
-            <div className="flex flex-row
-                            min-[461px]:justify-between 
-                            max-[460px]:gap-[36px]">
+            <div className="flex flex-row items-center h-[100%] overflow-hidden
+                            min-[460.02px]:justify-between 
+                            max-[460.01px]:gap-[36px]">
                 {/* Text */}
                 <p className="w-[171px] decoration-black font-poppins text-[16px] overflow-hidden
-                              max-[460px]:text-[14px]">Launch a project for your non profit!</p>
+                              max-[460.01px]:text-[14px]">Launch a project for your non profit!</p>
                 {/* BTN */}
-                <div className="bg-blueprint-deepblue w-[200px] h-[60px] max-[460px]:bg-red-700" ></div>
+                <div className="bg-blueprint-deepblue w-[200px] h-[60px] 
+                max-[460.01px]:bg-red-700" ></div>
             </div>
         </div>
         </>
