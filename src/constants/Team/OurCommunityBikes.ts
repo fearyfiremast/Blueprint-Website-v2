87c78c15e14a9869b621bde1340f2a532564ac84
fearyfiremast <fearyfiremast@gmail.com>
2026-04-17 18:18:27 -0700

    Feat: Added placeholder OurCommunityBikes team

---
+++ src/constants/Team/OurCommunityBikes.ts
@@ -0,0 +1,75 @@
+/**
+ * PLACEHOLDER VALUES
+ */
+export const OurCommunityBikes = [
+    {
+        title: "Steve B", 
+        role: "Designer", 
+        lastPosition: "designer", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Chris T", 
+        role: "project manager", 
+        lastPosition: "pm", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Sarah J", 
+        role: "developer", 
+        lastPosition: "dev", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Langley L", 
+        role: "executive", 
+        lastPosition: "exec", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Taylor S", 
+        role: "tech lead", 
+        lastPosition: "techLead", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Osmosis J", 
+        role: "Designer", 
+        lastPosition: "designer", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Cailey Z", 
+        role: "project manager", 
+        lastPosition: "pm", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Amanda Y", 
+        role: "developer", 
+        lastPosition: "dev", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "Elliott S", 
+        role: "executive", 
+        lastPosition: "exec", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+    {
+        title: "LongNameJane D", 
+        role: "tech lead", 
+        lastPosition: "techLead", 
+        img: "https://placehold.co/153x127", 
+        linkedin: "PLACEHOLDER-LINKEDIN"
+    },
+]
