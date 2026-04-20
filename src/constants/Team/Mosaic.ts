11ac9297022d0b2a0976e65be03e173bc66afcd0
fearyfiremast <fearyfiremast@gmail.com>
2026-04-20 16:27:51 -0700

    Fix: corrected linkedin URL

--- src/constants/Team/Mosaic.ts
+++ src/constants/Team/Mosaic.ts
@@ -4,42 +4,42 @@ export const Mosaic: MemberCardProps[] = [
   {
     name: "Peiman Zhiani",
     photoUrl: "Peiman.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/peiman-zhiani/",
+    linkedinUrl: "https://www.linkedin.com/in/peiman-zhiani/",
     roleType: "dev",
     role:"Senior Developer"
   },
   {
     name: "Johnson Luong",
     photoUrl: "Johnson.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/johnson-luong/",
+    linkedinUrl: "https://www.linkedin.com/in/johnson-luong/",
     roleType: "dev",
     role:"Senior Developer"
   },
   {
     name: "Duy Nguyen",
     photoUrl: "Duy.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/duwe-ng/",
+    linkedinUrl: "https://www.linkedin.com/in/duwe-ng/",
     roleType: "dev",
     role:"AI Developer"
   },
   {
     name: "Zahila Rehman",
     photoUrl: "Zahila.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/zahila-rehman-9118a4217/",
+    linkedinUrl: "https://www.linkedin.com/in/zahila-rehman-9118a4217/",
     roleType: "pm",
     role:"Project Manager"
   },
   {
     name: "Kasey Le",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/kasey-le-43aa31255/",
+    linkedinUrl: "https://www.linkedin.com/in/kasey-le-43aa31255/",
     roleType: "designer",
     role:"Designer"
   },
   {
     name: "Alison Lu",
     photoUrl: "Alison.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/alisonlu3/",
+    linkedinUrl: "https://www.linkedin.com/in/alisonlu3/",
     roleType: "designer",
     role:"Designer"
   },
@@ -53,49 +53,49 @@ export const Mosaic: MemberCardProps[] = [
   {
     name: "Kian Hoseinkhani",
     photoUrl: "Kian.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/kian-hosseinkhani-716a311b6/",
+    linkedinUrl: "https://www.linkedin.com/in/kian-hosseinkhani-716a311b6/",
     roleType: "pm",
     role:"Project Manager"
   },
   {
     name: "Enoch Muwanguzi",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/enoch-muwanguzi-42491425b/",
+    linkedinUrl: "https://www.linkedin.com/in/enoch-muwanguzi-42491425b/",
     roleType: "dev",
     role:"AI Developer"
   },
   {
     name: "Derek Huang",
     photoUrl: "Derek.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/derekhuang1/",
+    linkedinUrl: "https://www.linkedin.com/in/derekhuang1/",
     roleType: "dev",
     role:"Junior Developer"
   },
   {
     name: "Fabian Siswanto",
     photoUrl: "Fabian.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/fabiansiswanto/",
+    linkedinUrl: "https://www.linkedin.com/in/fabiansiswanto/",
     roleType: "dev",
     role:"Junior Developer"
   },
   {
     name: "Kousha Amouzesh",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/kousha-amouzesh/",
+    linkedinUrl: "https://www.linkedin.com/in/kousha-amouzesh/",
     roleType: "techLead",
     role:"Tech Lead"
   },
   {
     name: "Iris Au-Yeung",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/irisau-yeung/",
+    linkedinUrl: "https://www.linkedin.com/in/irisau-yeung/",
     roleType: "designer",
     role:"Designer"
   },
   {
     name: "Lauren Yip",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/lauren-yip/",
+    linkedinUrl: "https://www.linkedin.com/in/lauren-yip/",
     roleType: "dev",
     role:"Junior Developer"
   },
--- src/constants/Team/Pedals.ts
+++ src/constants/Team/Pedals.ts
@@ -5,107 +5,107 @@ export const Pedals:MemberCardProps[] = [
   {
     name: "Amy Zhao",
     photoUrl: "Amy.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/amy-zhao-/",
+    linkedinUrl: "https://www.linkedin.com/in/amy-zhao-/",
     roleType: "designer",
     role:"Designer"
   },
   {
     name: "Ayana Hussain",
     photoUrl: "Ayana.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/ayana-hussain-a3399a227/",
+    linkedinUrl: "https://www.linkedin.com/in/ayana-hussain-a3399a227/",
     roleType: "dev",
     role:"Senior Developer"
   },
   {
     name: "Vinh Dang",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/quang-vinh-dang-788ab0194/",
+    linkedinUrl: "https://www.linkedin.com/in/quang-vinh-dang-788ab0194/",
     roleType: "dev",
     role:"Senior Developer"
   },
   {
     name: "Eugene Zhu",
     photoUrl: "Eugene.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/eugenezhu/",
+    linkedinUrl: "https://www.linkedin.com/in/eugenezhu/",
     roleType: "designer",
     role:"Designer"
   },
   {
     name: "Kate Simone",
     photoUrl: "Kate.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/kate-simone-866335284/",
+    linkedinUrl: "https://www.linkedin.com/in/kate-simone-866335284/",
     roleType: "designer",
     role:"Designer"
   },
   {
     name: "Arthur Zhang",
     photoUrl: "Arthur.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/arthur0z/",
+    linkedinUrl: "https://www.linkedin.com/in/arthur0z/",
     roleType: "dev",
     role:"Senior Developer"
   },{
     name: "Ronney Lok",
     photoUrl: "Ronney.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/ronneylok/",
+    linkedinUrl: "https://www.linkedin.com/in/ronneylok/",
     roleType: "dev",
     role:"Senior Developer"
   },{
     name: "Sallin Koutev",
     photoUrl: "Sallin.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/sallin-koutev-216b0016b/",
+    linkedinUrl: "https://www.linkedin.com/in/sallin-koutev-216b0016b/",
     roleType: "techLead",
     role:"Tech Lead"
   },{
     name: "Caitlin Syho",
     photoUrl: "Caitlin.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/caitlinsyho/",
+    linkedinUrl: "https://www.linkedin.com/in/caitlinsyho/",
     roleType: "techLead",
     role:"Product Lead"
   },{
     name: "Hinako Kamiya",
     photoUrl: "Hinako.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/hinako-kamiya/",
+    linkedinUrl: "https://www.linkedin.com/in/hinako-kamiya/",
     roleType: "techLead",
     role:"Project Manager"
   },{
     name: "Duc Dang",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/ddang2001/",
+    linkedinUrl: "https://www.linkedin.com/in/ddang2001/",
     roleType: "techLead",
     role:"Tech Lead"
   },
   {
     name: "Tomi Lui",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/tomi-lui-3955631b3/",
+    linkedinUrl: "https://www.linkedin.com/in/tomi-lui-3955631b3/",
     roleType: "dev",
     role:"Senior Developer"
   },
   {
     name: "Terry Kwok",
     photoUrl: "Terry.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/lai-chit-kwok-6074472b5/",
+    linkedinUrl: "https://www.linkedin.com/in/lai-chit-kwok-6074472b5/",
     roleType: "dev",
     role:"Junior Developer"
   },
   {
     name: "Supriya Dua",
     photoUrl: "Supriya.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/supriya-dua-a8a576204/",
+    linkedinUrl: "https://www.linkedin.com/in/supriya-dua-a8a576204/",
     roleType: "dev",
     role:"Junior Developer"
   },
   {
     name: "Kumar Shivam",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/kumarshivam0002/",
+    linkedinUrl: "https://www.linkedin.com/in/kumarshivam0002/",
     roleType: "techLead",
     role:"Tech Lead"
   },
   {
     name: "Monishka Gautam",
     photoUrl: "blueprint.jpg",
-    linkedinUrl: "https://www.linkedinUrl.com/in/monishka-gautam/",
+    linkedinUrl: "https://www.linkedin.com/in/monishka-gautam/",
     roleType: "dev",
     role:"Junior Developer"
   },
