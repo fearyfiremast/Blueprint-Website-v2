99cff461d1c2e4f9867638104c6825e2e1355c85
melvikaf <melvikafa@gmail.com>
2026-06-16 16:36:06 -0700

    improve project page responsiveness and routing

--- src/components/projects-page/ProjectProjectCard.tsx
+++ src/components/projects-page/ProjectProjectCard.tsx
@@ -7,6 +7,7 @@ import React from 'react';
  * @param {string} project - Project key.
  * @returns {JSX.Element} JSX representation of the ProjectCard component.
  */
+import { Link } from "react-router-dom";
 
 export type ProjectCardProps = {
     logo_url: string;
@@ -17,13 +18,20 @@ export type ProjectCardProps = {
     client_name: string;
     service: string;
     sector: string;
+    href: string;
 }
 
 const ProjectCard = ({
-    logo_url="https://placehold.co/76x76", logo_url_alt="placeholder_alt", card_cover_url="https://placehold.co/517x354", 
-    card_cover_alt="placeholder_alt", description="reducing volunteer management time by 4 hours per month with a digital volunteer hour logbook", 
-    client_name="Our Community Bikes", service="Web-app", sector="Web-app"
-}:ProjectCardProps) => {
+    logo_url = "https://placehold.co/76x76",
+    logo_url_alt = "placeholder_alt",
+    card_cover_url = "https://placehold.co/517x354",
+    card_cover_alt = "placeholder_alt",
+    description = "reducing volunteer management time by 4 hours per month with a digital volunteer hour logbook",
+    client_name = "Our Community Bikes",
+    service = "Web-app",
+    sector = "Web-app",
+    href,
+}: ProjectCardProps) => {
     // if (!project || !project.tags) {
     //   return null; 
     // }
@@ -32,67 +40,69 @@ const ProjectCard = ({
     const COVER_BG_COLOUR = "gray-50";
     
     return (
-    <div className="px-9 pt-9 pb-12 bg-white rounded-[5px] inline-flex flex-col justify-start items-start gap-2.5 overflow-hidden 
-                    [@media(hover:hover)]:hover:ring-1 [@media(hover:hover)]:hover:ring-bp-grey [@media(hover:hover)]:hover:bg-bp-lightest-grey group
-                    md:w-auto md:max-w-[865px] md:min-w-[460px] md:px-9 md:pt-9 md:pb-12 md:h-[640px]
-                    max-[767px]:min-w-[276px] max-[767px]:max-w-[623px]">
-        <div className="self-stretch flex flex-col justify-start items-start gap-4 md:gap-5 ">
-            {/*  Hero Image  */}
-            <div className={`self-stretch h-40 bg-${COVER_BG_COLOUR} rounded-[5px] overflow-hidden md:h-80`}>
-                <img className='transition-transform duration-150 group-hover:scale-105 object-cover h-full' src={card_cover_url} alt={card_cover_alt}/>
-            </div>
-            
-            {/*  Title and Icons */}
-            <div className="self-stretch inline-flex justify-start items-start gap-3
-                            md:gap-[40px]">
-                <div className="flex-1 max-w-96  justify-start text-black text-lg  font-normal font-['Poppins'] leading-6 
-                                md:max-w-none md:text-[18px]/[140%] ">
-                    {description}
+    <Link to={href} className="block w-full no-underline text-inherit">
+        <div className="w-full px-9 pt-9 pb-12 bg-white rounded-[5px] inline-flex flex-col justify-start items-start gap-2.5 overflow-hidden 
+                        [@media(hover:hover)]:hover:ring-1 [@media(hover:hover)]:hover:ring-bp-grey [@media(hover:hover)]:hover:bg-bp-lightest-grey group
+                        md:w-full md:max-w-[865px] md:min-w-[460px] md:px-9 md:pt-9 md:pb-12 md:h-[640px]
+                        max-[767px]:min-w-[276px] max-[767px]:max-w-[623px]">
+            <div className="self-stretch flex flex-col justify-start items-start gap-4 md:gap-5 ">
+                {/*  Hero Image  */}
+                <div className={`self-stretch h-40 bg-${COVER_BG_COLOUR} rounded-[5px] overflow-hidden md:h-80`}>
+                    <img className='transition-transform duration-150 group-hover:scale-105 object-cover h-full' src={card_cover_url} alt={card_cover_alt}/>
                 </div>
-                { /* Left over from ProjectCard component. Not in design sheet so not sure why its here. Commented out in case it's important.
-                <div className="hidden w-20 h-20 bg-zinc-800 rounded-full md:block"></div> */}
-                <img className="max-[767px]:hidden w-[76px] h-[76px] md:block" src={logo_url} alt={logo_url_alt}/>
-            </div>
-            
-            {/*  Divider */}
-            <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-zinc-300"></div>
-            
-            {/* Info Section */}
-            <div className="w-full max-w-[552px] md:inline-flex md:flex-row justify-start items-start gap-3 md:gap-9">
-                <div className="flex flex-col justify-start items-start gap-1.5 
-                                md:max-w-[150px] md:gap-3
-                                max-[767px]:w-28">
-                    <div className="w-36 justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
-                        CLIENT
+                
+                {/*  Title and Icons */}
+                <div className="self-stretch inline-flex justify-start items-start gap-3
+                                md:gap-[40px]">
+                    <div className="flex-1 max-w-96  justify-start text-black text-lg  font-normal font-['Poppins'] leading-6 
+                                    md:max-w-none md:text-[18px]/[140%] ">
+                        {description}
                     </div>
-                    <div className="self-stretch justify-start md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
-                        {client_name}
-                    </div>
-                    </div>
-                <div className="flex flex-col justify-start items-start gap-1.5 
-                                md:max-w-[150px] md:gap-3
-                                max-[767px]:w-28">
-                    <div className="self-stretch justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
-                        SERVICE
-                    </div>
-                    <div className="self-stretch justify-start md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
-                        {service}
-                    </div>
+                    { /* Left over from ProjectCard component. Not in design sheet so not sure why its here. Commented out in case it's important.
+                    <div className="hidden w-20 h-20 bg-zinc-800 rounded-full md:block"></div> */}
+                    <img className="max-[767px]:hidden w-[76px] h-[76px] md:block" src={logo_url} alt={logo_url_alt}/>
                 </div>
-                <div className="flex flex-col justify-start items-start gap-1.5 
-                                md:max-w-[150px] md:gap-3 
-                                max-[767px]:w-28">
-                    <div className="self-stretch justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
-                        SECTOR
+                
+                {/*  Divider */}
+                <div className="self-stretch h-0 outline outline-1 outline-offset-[-0.50px] outline-zinc-300"></div>
+                
+                {/* Info Section */}
+                <div className="w-full max-w-[552px] md:inline-flex md:flex-row justify-start items-start gap-3 md:gap-9">
+                    <div className="flex flex-col justify-start items-start gap-1.5 
+                                    md:max-w-[150px] md:gap-3
+                                    max-[767px]:w-28">
+                        <div className="w-36 justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
+                            CLIENT
+                        </div>
+                        <div className="self-stretch justify-start md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
+                            {client_name}
+                        </div>
+                        </div>
+                    <div className="flex flex-col justify-start items-start gap-1.5 
+                                    md:max-w-[150px] md:gap-3
+                                    max-[767px]:w-28">
+                        <div className="self-stretch justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
+                            SERVICE
+                        </div>
+                        <div className="self-stretch justify-start md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
+                            {service}
+                        </div>
                     </div>
-                    <div className="justify-start md:self-stretch md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
-                        {sector}
+                    <div className="flex flex-col justify-start items-start gap-1.5 
+                                    md:max-w-[150px] md:gap-3 
+                                    max-[767px]:w-28">
+                        <div className="self-stretch justify-start text-neutral-500 text-[10px] md:text-sm font-medium font-['Poppins'] uppercase">
+                            SECTOR
+                        </div>
+                        <div className="justify-start md:self-stretch md:inline-flex text-zinc-800 text-sm md:text-base font-normal font-['Poppins']">
+                            {sector}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    );
+  </Link>
+);
 };
 
 export default ProjectCard;
